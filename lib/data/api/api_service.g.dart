68cc6bc7f81b74c46eb71ced366d3c028bd8c5ba
pallavipatade25 <patadepallavi254@gmail.com>
2026-01-27 10:45:37 +0530

    minor_changes

--- lib/data/api/api_service.g.dart
+++ lib/data/api/api_service.g.dart
@@ -232,34 +232,6 @@ class _ApiService implements ApiService {
   }
 
   @override
-  Future<dynamic> addProduct(Product product) async {
-    final _extra = <String, dynamic>{};
-    final queryParameters = <String, dynamic>{};
-    final _headers = <String, dynamic>{};
-    final _data = <String, dynamic>{};
-    _data.addAll(product.toJson());
-    final _options = _setStreamType<dynamic>(Options(
-      method: 'POST',
-      headers: _headers,
-      extra: _extra,
-    )
-        .compose(
-          _dio.options,
-          'insert/addProduct',
-          queryParameters: queryParameters,
-          data: _data,
-        )
-        .copyWith(
-            baseUrl: _combineBaseUrls(
-          _dio.options.baseUrl,
-          baseUrl,
-        )));
-    final _result = await _dio.fetch(_options);
-    final _value = _result.data;
-    return _value;
-  }
-
-  @override
   Future<List<EmployeeLogin>> getEmployeeList() async {
     final _extra = <String, dynamic>{};
     final queryParameters = <String, dynamic>{};
