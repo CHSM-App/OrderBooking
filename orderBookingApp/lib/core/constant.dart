e82ba6501365a94eac563a4c1037ca23109abc3f
Sagar D <dharaglkarsagar@gmail.com>
2026-02-26 18:29:16 +0530

    refactor: Clean up unused imports and commented code in interceptor

--- orderBookingApp/lib/core/constant.dart
+++ orderBookingApp/lib/core/constant.dart
@@ -1,5 +1,3 @@
-// const String baseUrl = 'http://192.168.1.7:8000/';
 const String baseUrl = 'https://orderbooking.vengurlatech.com/';
-// const String baseUrl = 'https://hospital-planning-gst-thriller.trycloudflare.com/';
 
 
